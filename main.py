53bdcc9354b4151a1fcdf12824618cd9c81a5957
Will G <williamgalbreath3@gmail.com>
2024-12-18 21:33:22 -0600

    Need to do the sort step now

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    
+    wordCount(file_contents)
+    
+    print(counter(file_contents))
+    
+
+def wordCount(file_contents):
+    words = file_contents.split()
+    count = len(words)
+    print(count)
+    
+def counter(file_contents):
+    chars = {}
+    for c in file_contents:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+        
+main()
