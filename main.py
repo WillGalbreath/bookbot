c0a95ea21aa4c47f7471037344b0a56e2c1ac56a
Will G <williamgalbreath3@gmail.com>
2024-12-18 21:42:51 -0600

    Think I'm finished

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 
 
 def main():
+    book_path = "books/frankenstein.txt"
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     
-    wordCount(file_contents)
+    count = wordCount(file_contents)
     
-    print(counter(file_contents))
+    unsort = (counter(file_contents))
+    
+    sorted = chars_sort(unsort)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{count} words found in the document")
+    print()
+    
+    for item in sorted:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
     
 
 def wordCount(file_contents):
     words = file_contents.split()
     count = len(words)
-    print(count)
+    return count
     
 def counter(file_contents):
     chars = {}
@@ -22,8 +36,18 @@ def counter(file_contents):
             chars[lowered] += 1
         else:
             chars[lowered] = 1
-    
+            
     return chars
 
+
+def sort_on(d):
+    return d["num"]
+
+def chars_sort(unsort):
+    sorted = []
+    for ch in unsort:
+        sorted.append({"char": ch, "num": unsort[ch]})
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
         
 main()
